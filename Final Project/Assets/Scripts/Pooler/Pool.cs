c4857cd77ca6ba33e4b93c073b79d4cca6091184
Pablo <pablma11@ucm.es>
2018-12-08 16:10:55 +0100

    Pooler seems to work

---
+++ Final Project/Assets/Scripts/Pooler/Pool.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Pool : NetworkBehaviour {
+
+    public int m_ObjectPoolSize = 10;
+    public GameObject m_Prefab;
+    public GameObject[] m_Pool;
+
+    public NetworkHash128 assetId { get; set; }
+
+    // Handles requests to spawn GameObjects on the client
+    public delegate GameObject SpawnDelegate(Vector3 position, NetworkHash128 assetId);
+
+    // Handles requests to unspawn GameObjects on the client
+    public delegate void UnSpawnDelegate(GameObject spawned);
+
+    public static Pool instance;
+
+    public bool poolerInitialized = false;
+    public bool serverHasStarted = false;
+
+
+    private void Awake()
+    {
+        instance = this;
+
+        assetId = m_Prefab.GetComponent<NetworkIdentity>().assetId;
+        m_Pool = new GameObject[m_ObjectPoolSize];
+        for (int i = 0; i < m_ObjectPoolSize; ++i)
+        {
+            //m_Pool[i] = (GameObject)Instantiate(m_Prefab, Vector3.zero, Quaternion.identity);
+            //m_Pool[i].name = "PoolObject" + i;
+            //m_Pool[i].SetActive(false);
+            CreatePooler(m_Pool, i);
+        }
+
+        ClientScene.RegisterSpawnHandler(assetId, SpawnObject, UnSpawnObject);
+
+        poolerInitialized = true;
+    }
+
+
+    private void CreatePooler(GameObject [] pool, int i)
+    {
+        pool[i] = (GameObject)Instantiate(m_Prefab, Vector3.zero, Quaternion.identity);
+        pool[i].name = "PoolObject" + i;
+        pool[i].SetActive(false);
+    }
+
+    public override void OnStartServer()
+    {
+        serverHasStarted = true;
+    }
+
+    public GameObject GetFromPool(Vector3 position)
+    {
+        foreach (var obj in m_Pool)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                //Debug.Log("Activating GameObject " + obj.name + " at " + position);
+                obj.transform.position = position;
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        Debug.Log("Could not grab GameObject from pool, nothing available");
+        return null;
+    }
+
+    public GameObject SpawnObject(Vector3 position, NetworkHash128 assetId)
+    {
+        return GetFromPool(position);
+    }
+
+    public void UnSpawnObject(GameObject spawned)
+    {
+        //Debug.Log("Re-pooling GameObject " + spawned.name);
+        spawned.SetActive(false);
+    }
+}
---
+++ Final Project/Assets/Scripts/Spawners/Spawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour {
+
+    public float TimeToRespawn = 2f;
+    private float timer;
+    private bool serverHasStarted = false;
+
+
+    public override void OnStartServer()
+    {
+        serverHasStarted = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    private void Update()
+    {
+        //if (Pool.instance.poolerInitialized)
+        //{
+        //    //pool = GameObject.Find("Pool").GetComponent<Pool>();
+        //    //if (isServer)
+        //    //    ServerSpawn();
+        //    //else
+        //    //CmdSpawn();
+
+        //    CmdServerSpawn();
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.L))
+        //    CmdSpawn();
+
+        if (serverHasStarted)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                CmdSpawn();
+                timer = TimeToRespawn;
+            }
+        }
+
+    }
+
+    [Command]
+    void CmdSpawn()
+    {
+        var duck = Pool.instance.GetFromPool(transform.position);
+        NetworkServer.Spawn(duck, Pool.instance.assetId);
+    }
+
+    [Command]
+    void CmdServerSpawn()
+    {
+        //var duck = Pool.instance.GetFromPool(transform.position);
+        //NetworkServer.Spawn(duck, Pool.instance.assetId);
+        //Pool.instance.GetFromPool(transform.position);
+        NetworkServer.Spawn(Pool.instance.SpawnObject(transform.position, Pool.instance.assetId), Pool.instance.assetId);
+    }
+}
